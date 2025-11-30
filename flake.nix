11cc172d333e8280fca74b1b3cf88a1945a47f4a
Gilles Habran <gilleshabran@protonmail.com>
2025-11-30 16:30:25 +0100

    init bazel project

--- flake.nix
+++ flake.nix
@@ -2,27 +2,21 @@
   description = "Monorepo dev environment with Bazel, Nix, and Kubernetes tooling";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # stable channel
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          # optionally avoid broken packages, reproducibility flags…
-        };
+        # stable nixpkgs
+        pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
           name = "dev-shell";
 
           buildInputs = [
             pkgs.bazel_7
-            pkgs.kubectl
-            pkgs.kustomize
-            pkgs.helm    
-            pkgs.jq
           ];
 
           shellHook = ''
