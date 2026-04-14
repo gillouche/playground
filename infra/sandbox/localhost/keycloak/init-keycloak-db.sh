96422a213aa540b4ae994d256220477e06915c47
Gilles Habran <gilleshabran@protonmail.com>
2026-04-14 23:47:29 +0200

    switch from gexec to create keycloak db

--- infra/sandbox/localhost/keycloak/init-keycloak-db.sh
+++ infra/sandbox/localhost/keycloak/init-keycloak-db.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    SELECT 'CREATE DATABASE keycloak' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
-EOSQL
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak'" | grep -q 1 || psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE keycloak"
